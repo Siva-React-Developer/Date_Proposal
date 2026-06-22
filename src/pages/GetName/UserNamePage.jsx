22988a8fc4d7b1afd4489e8ccd3e70e0910ce3ed
Siva-React-Developer <sivaramdasu35@gmail.com>
2026-06-22 15:22:43 +0530

    code added

--- src/pages/GetName/UserNamePage.jsx
+++ src/pages/GetName/UserNamePage.jsx
@@ -1,17 +1,21 @@
-import BackgroundVideo from "../../components/BackgroundVideo/BackgroundVideo"
+import BackgroundVideo from "../../components/BackgroundVideo/BackgroundVideo";
+import Style from "./userNamePage.module.css"
 
-const UserNamePage=()=>{
-    return(
-        <div>
-            <BackgroundVideo/>
-            <div>
-                <p>Every special story begins with a name.</p>
-                <p>Tell me yours, so I can make this moment even more special. ✨</p>
-            </div>
-            <form action="">
-                <input type="text" placeholder="Enter your beautiful name..."/>
-            </form>
+const UserNamePage = () => {
+  return (
+    <div>
+      <BackgroundVideo />
+      <div id={Style.container}>
+        <div id={Style.SentencesContainer}>
+          <p className={Style.SentencesContainer_info}>Every special story begins with a name.</p>
+          <p className={Style.SentencesContainer_info}>Tell me yours, so I can make this moment even more special. ✨</p>
         </div>
-    )
-}
-export default UserNamePage
+        <form id={Style.form} action="">
+          <input type="text" placeholder="Enter your beautiful name..." />
+          <button id={Style.continue}>➡️</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+export default UserNamePage;
